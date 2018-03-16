4f2500fad9a47d45e668c3bbdd604725f29d9601
olegchursin <olegchursin@gmail.com>
2018-03-16 13:35:11 -0400

    start React router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import './css/App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+// Components
 import DictionaryContainer from './components/dictionary/DictionaryContainer'
+import Main from './components/main/Main'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        <DictionaryContainer />
-        <Footer />
-      </div>
+      <Router>
+        <div>
+          <NavBar />
+          <Main />
+          <DictionaryContainer />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
