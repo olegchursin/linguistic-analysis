1f0f53f98c9cb7267d41cfca119e0d9e6196c334
Oleg Chursin <oleg.chursin@aon.com>
2022-10-13 16:30:47 -0400

    feat: add flowbite

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
+import { Alert } from 'flowbite-react';
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Alert color="info">Alert!</Alert>
+    </div>
+  );
 };
 
 export default Home;
